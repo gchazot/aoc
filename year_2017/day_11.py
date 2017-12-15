19ee731e0043341bc54fa12e0c6fbe4e202a3d4b
bhvr-gchazot <germain.chazot@bhvr.com>
2017-12-15 08:46:00 -0500

    2017 Day 11

---
+++ year_2017/day_11.py
@@ -0,0 +1,95 @@
+from unittest import TestCase
+import operator
+from aoc_utils import data_file
+
+
+class Coordinates(object):
+    def __init__(self, coordinates):
+        self.coordinates = coordinates[:]
+
+    def __str__(self):
+        return "{}".format(self.coordinates)
+
+    def __getitem__(self, item):
+        return self.coordinates[item]
+
+    def __len__(self):
+        return len(self.coordinates)
+
+    def __add__(self, other):
+        new_coordinates = map(operator.add, self.coordinates, other.coordinates)
+        return Coordinates(new_coordinates)
+
+    def __iadd__(self, other):
+        self.coordinates = map(operator.add, self.coordinates, other.coordinates)
+        return self
+
+    def __eq__(self, other):
+        return all(map(operator.eq, self.coordinates, other.coordinates))
+
+
+directions = {
+    "n": Coordinates([0, 1, 1]),
+    "s": Coordinates([0, -1, -1]),
+    "ne": Coordinates([1, 1, 0]),
+    "se": Coordinates([1, 0, -1]),
+    "nw": Coordinates([-1, 0, 1]),
+    "sw": Coordinates([-1, -1, 0])
+}
+
+
+class GridWalker:
+    def __init__(self):
+        self.position = Coordinates([0, 0, 0])
+        self.furthest = 0
+
+    def walk(self, route):
+        for coord in route.split(","):
+            step = directions[coord]
+            self.position += step
+            self.furthest = max(self.furthest, self.distance())
+
+    def distance(self):
+        return max(map(abs, self.position.coordinates))
+
+
+def final_coordinates(route):
+    walker = GridWalker()
+    walker.walk(route)
+    return walker.position
+
+
+def shortest_distance(route):
+    walker = GridWalker()
+    walker.walk(route)
+    return walker.distance()
+
+
+class TestHexWalk(TestCase):
+    def setUp(self):
+        def assert_coordinates_equal(a, b, msg=None):
+            if msg is None:
+                msg = "Expected: {}\nActual: {}".format(a, b)
+            if not a == b:
+                raise self.failureException(msg)
+
+        self.addTypeEqualityFunc(Coordinates, assert_coordinates_equal)
+
+    def test_final_coordinates(self):
+        self.assertEquals(Coordinates([3, 3, 0]), final_coordinates("ne,ne,ne"))
+        self.assertEquals(Coordinates([0, 0, 0]), final_coordinates("ne,ne,sw,sw"))
+        self.assertEquals(Coordinates([2, 0, -2]), final_coordinates("ne,ne,s,s"))
+        self.assertEquals(Coordinates([-1, -3, -2]), final_coordinates("se,sw,se,sw,sw"))
+
+    def test_hexample(self):
+        self.assertEqual(3, shortest_distance("ne,ne,ne"))
+        self.assertEqual(0, shortest_distance("ne,ne,sw,sw"))
+        self.assertEqual(2, shortest_distance("ne,ne,s,s"))
+        self.assertEqual(3, shortest_distance("se,sw,se,sw,sw"))
+
+    def test_mine(self):
+        with open(data_file(2017, "day_11_mine.txt")) as f:
+            walker = GridWalker()
+            walker.walk(f.read())
+            self.assertEqual(687, walker.distance())
+            self.assertEqual(1483, walker.furthest)
