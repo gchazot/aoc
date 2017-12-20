ac16dd0ee5266ec38ddb3ab914f13e0db0ec7239
Germain CHAZOT <g.chazot@gmail.com>
2017-12-20 00:37:00 -0500

    2017 day 15 part 2

--- year_2017/day_15.py
+++ year_2017/day_15.py
@@ -2,9 +2,10 @@ import unittest
 
 
 class Generator:
-    def __init__(self, start, factor):
+    def __init__(self, start, factor, criterion):
         self.value = start
         self.factor = factor
+        self.criterion = criterion
 
     DIVIDER = 2147483647
     MASK = (1 << 16) - 1
@@ -15,6 +16,14 @@ class Generator:
     @property
     def hash(self):
         return self.value & self.MASK
+
+    def gen_values(self, num_values):
+        i = 0
+        while i < num_values:
+            self.step()
+            if self.value % self.criterion == 0:
+                yield self.value
+                i += 1
 
 
 def compare_generators(gen_a, gen_b):
@@ -31,14 +40,19 @@ def count_matches(gen_a, gen_b, iterations):
     return count
 
 
+def count_picky_matches(gen_a, gen_b, iterations):
+    values = zip(gen_a.gen_values(iterations), gen_b.gen_values(iterations))
+    return sum((1 for pair in values if (pair[0] - pair[1]) & Generator.MASK == 0))
+
+
 class TestGenerator(unittest.TestCase):
     def use_example_gens(self):
-        self.genA = Generator(65, 16807)
-        self.genB = Generator(8921, 48271)
+        self.genA = Generator(65, 16807, 4)
+        self.genB = Generator(8921, 48271, 8)
 
     def use_my_generators(self):
-        self.genA = Generator(116, 16807)
-        self.genB = Generator(299, 48271)
+        self.genA = Generator(116, 16807, 4)
+        self.genB = Generator(299, 48271, 8)
 
     def step_generators(self):
         self.genA.step()
@@ -103,3 +117,27 @@ class TestGenerator(unittest.TestCase):
     def test_count_comparisons_mine(self):
         self.use_my_generators()
         self.assertEqual(569, count_matches(self.genA, self.genB, 40000000))
+
+    def test_gen_next_picky_value_examples(self):
+        self.use_example_gens()
+
+        values = zip(self.genA.gen_values(5), self.genB.gen_values(5))
+        self.assertListEqual([
+            (1352636452, 1233683848),
+            (1992081072, 862516352),
+            (530830436, 1159784568),
+            (1980017072, 1616057672),
+            (740335192, 412269392),
+        ], list(values))
+
+    @unittest.skip("Taking too long for now")
+    def test_count_picky_comparisons_example(self):
+        self.use_example_gens()
+
+        self.assertEqual(309, count_picky_matches(self.genA, self.genB, 5000000))
+
+    @unittest.skip("Taking too long for now")
+    def test_count_picky_comparisons_mine(self):
+        self.use_my_generators()
+
+        self.assertEqual(298, count_picky_matches(self.genA, self.genB, 5000000))
