58a1094c12efa477c71802e1e1663d91d5a35b64
Germain Chazot <germain@edited.com>
2018-12-31 17:37:55 +0000

    2018 Day 03 - slight optimisation

--- year_2018/day_03.py
+++ year_2018/day_03.py
@@ -60,17 +60,15 @@ class TestClaimsList(unittest.TestCase):
 
 class ClaimsList:
     def __init__(self):
-        self._claims = {}
-        self._claim_grid = collections.defaultdict(list)
+        self._claims = set()
+        self._claim_grid = ClaimsGrid()
 
     def add(self, claim_string):
         claim_id, left, top, width, height = self.parse_claim(claim_string)
         right = left + width
         bottom = top + height
-        self._claims[claim_id] = (left, right, top, bottom)
-        for x in range(left, right):
-            for y in range(top, bottom):
-                self._claim_grid[x, y].append(claim_id)
+        self._claims.add(claim_id)
+        self._claim_grid.add_claim(claim_id, left, right, top, bottom)
 
     def size(self):
         return len(self._claims)
@@ -83,18 +81,19 @@ class ClaimsList:
         return count
 
     def find_non_overlapped(self):
-        claim_ids = set(self._claims.keys())
+        claim_ids = self._claims.copy()
         for claims in self._claim_grid.values():
             if len(claims) > 1:
                 claim_ids.difference_update(claims)
         return list(claim_ids)
 
+    CLAIM_PATTERN = re.compile(
+        r"^#(?P<claim_id>\d+) @ (?P<left>\d+),(?P<top>\d+): (?P<width>\d+)x(?P<height>\d+)$"
+    )
+
     @staticmethod
     def parse_claim(claim_string):
-        claim_re = re.compile(
-            "^#(?P<claim_id>\d+) @ (?P<left>\d+),(?P<top>\d+): (?P<width>\d+)x(?P<height>\d+)$"
-        )
-        match = claim_re.match(claim_string)
+        match = ClaimsList.CLAIM_PATTERN.match(claim_string)
         if match is None:
             return None
 
@@ -108,3 +107,16 @@ class ClaimsList:
             int_for("width"),
             int_for("height"),
         )
+
+
+class ClaimsGrid:
+    def __init__(self):
+        self._grid = collections.defaultdict(list)
+
+    def add_claim(self, claim_id, left, right, top, bottom):
+        for x in range(left, right):
+            for y in range(top, bottom):
+                self._grid[x, y].append(claim_id)
+
+    def values(self):
+        return self._grid.values()
