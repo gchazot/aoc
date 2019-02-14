57dcbb8473c15c50580352385663c77b7087fe7e
Germain CHAZOT <g.chazot@gmail.com>
2019-02-14 12:24:25 +0000

    Add utils to read lines from example files

--- aoc_utils.py
+++ aoc_utils.py
@@ -1,7 +1,21 @@
 import os
+from contextlib import contextmanager
 
 
 def data_file(year, filename):
     root_dir = os.path.dirname(os.path.normpath(__file__))
     year_dir = "year_{}".format(year)
     return os.path.join(root_dir, year_dir, "data", filename)
+
+
+@contextmanager
+def open_data_file(year, filename):
+    file_path = data_file(year, filename)
+    with open(file_path) as fh:
+        yield fh
+
+
+def data_lines(year, filename):
+    with open_data_file(year, filename) as fh:
+        for line in fh.readlines():
+            yield line.strip('\n')
