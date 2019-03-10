c10ad9bde71c37e54424601c3ace05d37c740b55
Germain Chazot <germain@edited.com>
2019-03-10 13:32:11 +0000

    Add methods to find targets for attack and movement

--- year_2018/day_15.py
+++ year_2018/day_15.py
@@ -94,10 +94,33 @@ class TestCaves(unittest.TestCase):
         caves = self.make_default_caves()
         self.assertListEqual([(1, 1), (4, 1), (2, 3), (5, 3)], caves._iterate_units())
 
-    def test_find_next_target(self):
-        caves = self.make_default_caves()
+    def test_get_attack_target(self):
+        caves_2 = Caves([
+            "#######",
+            "#..EG.#",
+            "#...#.#",
+            "#.G.#G#",
+            "#######",
+        ])
+        self.assertEqual((4, 1), caves_2.get_attack_target((3, 1), 'E'))
+        self.assertEqual((3, 1), caves_2.get_attack_target((4, 1), 'G'))
+        self.assertEqual(None, caves_2.get_attack_target((2, 3), 'G'))
+        self.assertEqual(None, caves_2.get_attack_target((5, 3), 'G'))
 
-        self.assertEqual((3, 1), caves._find_next_target((1, 1), 'E'))
+    def test_find_next_destination(self):
+        caves = self.make_default_caves()
+        self.assertEqual((3, 1), caves.find_next_destination((1, 1), 'E'))
+        self.assertEqual((2, 1), caves.find_next_destination((4, 1), 'G'))
+
+        caves_2 = Caves([
+            "#######",
+            "#..EG.#",
+            "#...#.#",
+            "#.G.#G#",
+            "#######",
+        ])
+        self.assertEqual((2, 2), caves_2.find_next_destination((3, 1), 'E'))
+        self.assertEqual(None, caves_2.find_next_destination((4, 1), 'G'))
 
 
 TEAMS = {'E', 'G'}
@@ -113,9 +136,16 @@ class Caves:
             if entry in TEAMS:
                 self.fighters[entry][position] = 200
 
-    def _find_next_target(self, unit, team):
-        targets = self.get_targets(team)
-        all_closest = self._find_all_closest(unit, targets)
+    def get_attack_target(self, unit, team):
+        for adjacent in self.get_coordinates_around(unit):
+            if self._caves[adjacent] not in [EMPTY_VALUE, team]:
+                return adjacent
+
+    def find_next_destination(self, unit, team):
+        in_range = self.get_in_range(team)
+        if not in_range or unit in in_range:
+            return None
+        all_closest = self._find_all_closest(unit, in_range)
         return self._solve_tie(all_closest)
 
     def _find_all_closest(self, from_coords, targets):
