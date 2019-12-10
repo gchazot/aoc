d5552bb2761a6f8947952db3f1feba83afa92c74
Germain Chazot <germain@edited.com>
2019-12-10 18:37:05 +0000

    2019 - Move processor tests to processor file

--- year_2019/day_02.py
+++ year_2019/day_02.py
@@ -6,29 +6,6 @@ from year_2019.int_code_processor import IntCodeProcessor, instructions_day_02
 
 
 class TestIntCodeProcessor(unittest.TestCase):
-    def test_initialise(self):
-        processor = IntCodeProcessor([1, 2, 3, 4, 0], instructions_day_02)
-        self.assertListEqual([1, 2, 3, 4, 0], processor.memory)
-        self.assertEqual(instructions_day_02, processor.instructions)
-
-    def test_execute_example_instructions(self):
-        def check_instruction(expected_memory, address, initial_state):
-            processor = IntCodeProcessor(initial_state, instructions_day_02)
-            processor.execute_instruction_at(address)
-            self.assertListEqual(expected_memory, processor.memory)
-
-        check_instruction([2, 0, 0, 0, 99], 0, [1, 0, 0, 0, 99])
-        check_instruction([2, 3, 0, 6, 99], 0, [2, 3, 0, 3, 99])
-        check_instruction([2, 4, 4, 5, 99, 9801], 0, [2, 4, 4, 5, 99, 0])
-
-        check_instruction([1, 1, 1, 4, 2, 5, 6, 0, 99], 0, [1, 1, 1, 4, 99, 5, 6, 0, 99])
-        check_instruction([30, 1, 1, 4, 2, 5, 6, 0, 99], 4, [1, 1, 1, 4, 2, 5, 6, 0, 99])
-
-    def test_execute_example(self):
-        processor = IntCodeProcessor([1, 1, 1, 4, 99, 5, 6, 0, 99], instructions_day_02)
-        processor.execute()
-        self.assertListEqual([30, 1, 1, 4, 2, 5, 6, 0, 99], processor.memory)
-
     def test_execute_mine(self):
         initial_memory = list(map(int, data_text(2019, "day_02_mine.txt").split(",")))
         initial_memory[1] = 12
--- year_2019/int_code_processor.py
+++ year_2019/int_code_processor.py
@@ -1,4 +1,31 @@
+import unittest
 import operator
+
+
+class TestIntCodeProcessor(unittest.TestCase):
+    def test_initialise(self):
+        fake_instructions = {42: 'blah'}
+        processor = IntCodeProcessor([1, 2, 3, 4, 0], fake_instructions)
+        self.assertListEqual([1, 2, 3, 4, 0], processor.memory)
+        self.assertEqual(fake_instructions, processor.instructions)
+
+    def test_execute_example_instructions(self):
+        def check_instruction(expected_memory, address, initial_state):
+            processor = IntCodeProcessor(initial_state, instructions_day_02)
+            processor.execute_instruction_at(address)
+            self.assertListEqual(expected_memory, processor.memory)
+
+        check_instruction([2, 0, 0, 0, 99], 0, [1, 0, 0, 0, 99])
+        check_instruction([2, 3, 0, 6, 99], 0, [2, 3, 0, 3, 99])
+        check_instruction([2, 4, 4, 5, 99, 9801], 0, [2, 4, 4, 5, 99, 0])
+
+        check_instruction([1, 1, 1, 4, 2, 5, 6, 0, 99], 0, [1, 1, 1, 4, 99, 5, 6, 0, 99])
+        check_instruction([30, 1, 1, 4, 2, 5, 6, 0, 99], 4, [1, 1, 1, 4, 2, 5, 6, 0, 99])
+
+    def test_execute_example(self):
+        processor = IntCodeProcessor([1, 1, 1, 4, 99, 5, 6, 0, 99], instructions_day_02)
+        processor.execute()
+        self.assertListEqual([30, 1, 1, 4, 2, 5, 6, 0, 99], processor.memory)
 
 
 class EndProgram(Exception):
