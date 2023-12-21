45c052ae2837c627a4032571625d055c41e1d65f
Germain Chazot <germain@edited.com>
2023-12-21 10:02:18 +0100

    2023 Day 9 part 2

--- year_2023/src/day9/mod.rs
+++ year_2023/src/day9/mod.rs
@@ -1,4 +1,5 @@
 use crate::utils;
+use std::collections::VecDeque;
 
 #[test]
 fn test_mine() {
@@ -7,31 +8,43 @@ fn test_mine() {
 
 pub fn execute() {
     let readings = Reading::from_file("mine.txt");
-    assert_eq!(1772145754, extrapolate_all(readings));
+    assert_eq!((1772145754, 867), extrapolate_all(readings));
 }
 
 #[test]
 fn test_extrapolate_all() {
     let readings = Reading::from_file("example.txt");
-    assert_eq!(114, extrapolate_all(readings));
+    assert_eq!((114, 2), extrapolate_all(readings));
 }
 
-fn extrapolate_all(mut readings: Vec<Reading>) -> Value {
-    readings
-        .iter_mut()
-        .map(|reading| {
-            reading.extrapolate();
-            reading.values.last().unwrap()
-        })
-        .sum()
+fn extrapolate_all(mut readings: Vec<Reading>) -> (Value, Value) {
+    let mut total_right = 0 as Value;
+    let mut total_left = 0 as Value;
+
+    for reading in readings.iter_mut() {
+        reading.extrapolate();
+        total_right += reading.last();
+        total_left += reading.first();
+    }
+
+    (total_right, total_left)
 }
 
 #[test]
 fn test_extrapolate() {
     let mut reading1 = Reading::from_line("0 3 6 9 12 15");
     reading1.extrapolate();
-    assert_eq!(7, reading1.values.len());
-    assert_eq!(18, *reading1.values.last().unwrap());
+
+    assert_eq!(8, reading1.values.len());
+    assert_eq!(18, reading1.values[7]);
+    assert_eq!(-3, reading1.values[0]);
+
+    let mut reading2 = Reading::from_line("10 13 16 21 30 45");
+    reading2.extrapolate();
+
+    assert_eq!(8, reading2.values.len());
+    assert_eq!(68, reading2.values[7]);
+    assert_eq!(5, reading2.values[0]);
 }
 
 #[test]
@@ -60,38 +73,39 @@ fn test_is_zeros() {
 fn test_derive_reading() {
     let reading1 = Reading::from_line("0 3 6 9 12 15");
     let derivative1_1 = reading1.derive();
-    assert_eq!(vec![3; 5], derivative1_1.values);
+    assert_eq!(Values::from([3; 5]), derivative1_1.values);
     let derivative1_2 = derivative1_1.derive();
-    assert_eq!(vec![0; 4], derivative1_2.values);
+    assert_eq!(Values::from([0; 4]), derivative1_2.values);
 
     let reading2 = Reading::from_line("1 3 6 10 15 21");
     let derivative2_1 = reading2.derive();
-    assert_eq!(vec![2, 3, 4, 5, 6], derivative2_1.values);
+    assert_eq!(Values::from([2, 3, 4, 5, 6]), derivative2_1.values);
     let derivative2_2 = derivative2_1.derive();
-    assert_eq!(vec![1; 4], derivative2_2.values);
+    assert_eq!(Values::from([1; 4]), derivative2_2.values);
     let derivative2_3 = derivative2_2.derive();
-    assert_eq!(vec![0; 3], derivative2_3.values);
+    assert_eq!(Values::from([0; 3]), derivative2_3.values);
 }
 
 #[test]
 fn test_parse_readings() {
     let readings = Reading::from_file("example.txt");
     assert_eq!(3, readings.len());
-    assert_eq!(vec![0, 3, 6, 9, 12, 15], readings[0].values);
-    assert_eq!(vec![1, 3, 6, 10, 15, 21], readings[1].values);
-    assert_eq!(vec![10, 13, 16, 21, 30, 45], readings[2].values);
+    assert_eq!(Values::from([0, 3, 6, 9, 12, 15]), readings[0].values);
+    assert_eq!(Values::from([1, 3, 6, 10, 15, 21]), readings[1].values);
+    assert_eq!(Values::from([10, 13, 16, 21, 30, 45]), readings[2].values);
 }
 
 #[test]
 fn test_parse_reading() {
     let reading = Reading::from_line("0 3 6 9 -12 15");
-    assert_eq!(vec![0, 3, 6, 9, -12, 15], reading.values);
+    assert_eq!(Values::from([0, 3, 6, 9, -12, 15]), reading.values);
 }
 
 type Value = i32;
+type Values = VecDeque<Value>;
 
 struct Reading {
-    values: Vec<Value>,
+    values: Values,
 }
 
 impl Reading {
@@ -116,19 +130,31 @@ impl Reading {
         Reading { values }
     }
 
+    fn first(&self) -> Value {
+        self.values[0]
+    }
+    fn last(&self) -> Value {
+        self.values[self.values.len() - 1]
+    }
+
     fn extrapolate(&mut self) {
         let mut derivatives = self.derive_fully();
 
-        let mut increment = 0 as Value;
-        for i in 0..derivatives.len() {
-            let j = derivatives.len() - 1 - i;
-            let last_value = derivatives[j].values.last().unwrap();
-            increment = last_value + increment;
-            derivatives[j].values.push(increment);
+        let mut increment_back = 0 as Value;
+        let mut increment_front = 0 as Value;
+        for derivative in derivatives.iter_mut().rev() {
+            increment_back = derivative.last() + increment_back;
+            derivative.values.push_back(increment_back);
+
+            increment_front = derivative.first() - increment_front;
+            derivative.values.push_front(increment_front);
         }
 
-        let last_value = self.values.last().unwrap();
-        self.values.push(last_value + increment);
+        let last_value = self.last();
+        self.values.push_back(last_value + increment_back);
+
+        let first_value = self.first();
+        self.values.push_front(first_value - increment_front);
     }
 
     fn derive_fully(&self) -> Vec<Reading> {
