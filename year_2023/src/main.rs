d394a1eb4d16b286dc0953e6fcc4ba51472fa806
Germain Chazot <g.chazot@gmail.com>
2024-10-30 14:58:04 +0100

    2023 Day 13

--- year_2023/src/main.rs
+++ year_2023/src/main.rs
@@ -2,6 +2,7 @@ mod day1;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 mod day2;
 mod day3;
 mod day4;
@@ -25,4 +26,5 @@ fn main() {
     day10::execute();
     day11::execute();
     day12::execute();
+    day13::execute();
 }
