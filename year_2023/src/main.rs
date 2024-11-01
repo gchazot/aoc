effecd326226020688d18da4e37fc08d70307860
Germain Chazot <g.chazot@gmail.com>
2024-11-01 01:14:40 +0100

    2023 Day 16 Not very performant

--- year_2023/src/main.rs
+++ year_2023/src/main.rs
@@ -5,6 +5,7 @@ mod day12;
 mod day13;
 mod day14;
 mod day15;
+mod day16;
 mod day2;
 mod day3;
 mod day4;
@@ -31,4 +32,5 @@ fn main() {
     day13::execute();
     day14::execute();
     day15::execute();
+    day16::execute();
 }
