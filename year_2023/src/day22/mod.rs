5cc54253c933e65d39270ff829604ff4c9b91b38
Germain Chazot <germain@edited.com>
2024-11-18 22:01:33 +0100

    2023 Day 22 part 2

--- year_2023/src/day22/mod.rs
+++ year_2023/src/day22/mod.rs
@@ -1,5 +1,5 @@
 use crate::utils;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 #[test]
 fn test_mine() {
@@ -14,9 +14,12 @@ pub fn execute() {
 
     let disintegratable = mine.disintegratable_bricks();
     assert_eq!(389, disintegratable.len());
+
+    let chain_reactions = mine.chain_reactions();
+    assert_eq!(70609, chain_reactions.values().sum::<usize>());
 }
 
-type Dimension = i16;
+type Dimension = u16;
 
 #[derive(Debug, Hash, Clone, PartialEq, Eq)]
 struct Coordinates {
@@ -39,16 +42,18 @@ impl Coordinates {
     }
 }
 
+type BrickID = u32;
+
 #[derive(Debug, Hash, Clone, PartialEq, Eq)]
 struct Brick {
-    id: usize,
+    id: BrickID,
     start: Coordinates,
     end: Coordinates,
     blocks: Vec<Coordinates>,
 }
 
 impl Brick {
-    fn new(id: usize, start: Coordinates, end: Coordinates) -> Brick {
+    fn new(id: BrickID, start: Coordinates, end: Coordinates) -> Brick {
         let mut blocks = Vec::new();
         for x in start.x..end.x + 1 {
             for y in start.y..end.y + 1 {
@@ -64,7 +69,7 @@ impl Brick {
             blocks,
         }
     }
-    fn from_line(id: usize, s: &String) -> Brick {
+    fn from_line(id: BrickID, s: &String) -> Brick {
         let (start_str, end_str) = s.split_once("~").unwrap();
         let start = Coordinates::from_line(start_str.to_string());
         let end = Coordinates::from_line(end_str.to_string());
@@ -138,7 +143,7 @@ impl BrickYard {
         let mut bricks = lines
             .iter()
             .enumerate()
-            .map(|(id, line)| Brick::from_line(id, line))
+            .map(|(id, line)| Brick::from_line(id as BrickID, line))
             .collect::<Vec<_>>();
         bricks.sort_by_key(|brick| brick.start.z);
 
@@ -181,19 +186,20 @@ impl BrickYard {
         }
     }
 
-    fn disintegratable_bricks(&self) -> HashSet<Brick> {
+    fn disintegratable_bricks(&self) -> HashSet<BrickID> {
         let mut cannot_be_disintegrated = HashSet::new();
         for brick in self.bricks.iter() {
             let mut supported_by = HashSet::new();
             for other_brick in self.bricks.iter() {
-                if brick != other_brick {
+                if brick.id != other_brick.id {
                     for base in brick.blocks.iter().map(|block| Coordinates {
                         x: block.x,
                         y: block.y,
                         z: brick.start.z - 1,
                     }) {
                         if other_brick.blocks.contains(&base) {
-                            supported_by.insert(other_brick);
+                            supported_by.insert(other_brick.id);
+                            break;
                         }
                     }
                 }
@@ -205,14 +211,84 @@ impl BrickYard {
             }
         }
 
-        let mut can_be_disintegrated = HashSet::new();
+        let all_ids = self
+            .bricks
+            .iter()
+            .map(|brick| brick.id)
+            .collect::<HashSet<BrickID>>();
+        let can_be_disintegrated = all_ids
+            .difference(&cannot_be_disintegrated)
+            .cloned()
+            .collect();
+
+        can_be_disintegrated
+    }
+
+    fn chain_reactions(&self) -> HashMap<BrickID, usize> {
+        let mut supports = HashMap::<BrickID, Vec<BrickID>>::new();
+
         for brick in self.bricks.iter() {
-            if !cannot_be_disintegrated.contains(brick) {
-                can_be_disintegrated.insert(brick.clone());
+            for other_brick in self.bricks.iter() {
+                supports.entry(other_brick.id).or_insert(vec![]);
+                if brick.id != other_brick.id {
+                    for base in brick.blocks.iter().map(|block| Coordinates {
+                        x: block.x,
+                        y: block.y,
+                        z: brick.start.z - 1,
+                    }) {
+                        if other_brick.blocks.contains(&base) {
+                            supports.entry(other_brick.id).and_modify(|v| {
+                                v.push(brick.id);
+                            });
+                            break;
+                        }
+                    }
+                }
             }
         }
 
-        can_be_disintegrated
+        let mut reactions = HashMap::new();
+        for brick in self.bricks.iter() {
+            let mut still_supports = supports.clone();
+
+            let mut to_remove = VecDeque::from([brick.id]);
+            while to_remove.len() > 0 {
+                let id = to_remove.pop_front().unwrap();
+                let all_supported = still_supports.remove(&id);
+                if all_supported.is_some() {
+                    for supported in all_supported.unwrap() {
+                        if still_supports
+                            .values()
+                            .find(|still_supported| still_supported.contains(&supported))
+                            .is_none()
+                        {
+                            to_remove.push_back(supported);
+                        }
+                    }
+                }
+            }
+
+            let mut still_there =
+                still_supports
+                    .iter()
+                    .fold(HashSet::new(), |mut acc, (&id, supported)| {
+                        if id != brick.id {
+                            acc.insert(id);
+                        }
+                        for &id in supported {
+                            if id != brick.id {
+                                acc.insert(id);
+                            }
+                        }
+                        acc
+                    });
+
+            let unsupported = self.bricks.len() - 1 - still_there.len();
+
+            reactions.insert(brick.id, unsupported);
+        }
+
+        reactions
     }
 }
 #[test]
@@ -241,7 +317,7 @@ fn test_drop() {
         .bricks
         .iter()
         .enumerate()
-        .all(|(index, brick)| index == brick.id));
+        .all(|(index, brick)| index as BrickID == brick.id));
 
     assert_eq!(Coordinates { x: 1, y: 0, z: 1 }, example.bricks[0].start);
     assert_eq!(Coordinates { x: 1, y: 2, z: 1 }, example.bricks[0].end);
@@ -272,4 +348,23 @@ fn test_destroy_one() {
     let destroyables = example.disintegratable_bricks();
 
     assert_eq!(5, destroyables.len());
+}
+
+#[test]
+fn test_chain_reactions() {
+    let mut example = BrickYard::from_lines(_example());
+    example.drop();
+
+    let chain_reactions = example.chain_reactions();
+
+    assert_eq!(7, chain_reactions.len());
+    assert_eq!(7, chain_reactions.values().sum::<usize>());
+
+    assert_eq!(6, chain_reactions[&0]);
+    assert_eq!(0, chain_reactions[&1]);
+    assert_eq!(0, chain_reactions[&2]);
+    assert_eq!(0, chain_reactions[&3]);
+    assert_eq!(0, chain_reactions[&4]);
+    assert_eq!(1, chain_reactions[&5]);
+    assert_eq!(0, chain_reactions[&6]);
 }
