c351db66638295926dd4c0c44c16e29e974fc6ad
Germain Chazot <germain@edited.com>
2024-11-19 13:19:31 +0100

    2023 Day 23 part 2 - too slow for solution

--- year_2023/src/day23/mod.rs
+++ year_2023/src/day23/mod.rs
@@ -7,10 +7,19 @@ fn test_mine() {
 }
 
 pub fn execute() {
-    let mine = Map::from_lines(utils::read_lines("src/day23/mine.txt"));
-    let paths = find_paths(&mine);
+    let mine_slippery = Map::from_lines(utils::read_lines("src/day23/mine.txt"), true);
+    let slippery_paths = find_paths(&mine_slippery);
+    assert_eq!(
+        2394,
+        slippery_paths.iter().map(|p| p.len() - 1).max().unwrap()
+    );
 
-    assert_eq!(2394, paths.iter().map(|p| p.len() - 1).max().unwrap());
+    // let mine_sticky = Map::from_lines(utils::read_lines("src/day23/mine.txt"), false);
+    // let sticky_paths = find_paths(&mine_sticky);
+    // assert_eq!(
+    //     2394,
+    //     sticky_paths.iter().map(|p| p.len() - 1).max().unwrap()
+    // );
 }
 
 #[derive(Clone, Eq, PartialEq, Debug)]
@@ -35,7 +44,7 @@ struct Map {
 }
 
 impl Map {
-    fn from_lines(lines: Vec<String>) -> Map {
+    fn from_lines(lines: Vec<String>, with_slopes: bool) -> Map {
         let tiles: Vec<Vec<Tile>> = lines
             .iter()
             .map(|line| {
@@ -43,10 +52,34 @@ impl Map {
                     .map(|c| match c {
                         '#' => Tile::Forest,
                         '.' => Tile::Path,
-                        '<' => Tile::Slope(Direction::Left),
-                        '>' => Tile::Slope(Direction::Right),
-                        '^' => Tile::Slope(Direction::Up),
-                        'v' => Tile::Slope(Direction::Down),
+                        '<' => {
+                            if with_slopes {
+                                Tile::Slope(Direction::Left)
+                            } else {
+                                Tile::Path
+                            }
+                        }
+                        '>' => {
+                            if with_slopes {
+                                Tile::Slope(Direction::Right)
+                            } else {
+                                Tile::Path
+                            }
+                        }
+                        '^' => {
+                            if with_slopes {
+                                Tile::Slope(Direction::Up)
+                            } else {
+                                Tile::Path
+                            }
+                        }
+                        'v' => {
+                            if with_slopes {
+                                Tile::Slope(Direction::Down)
+                            } else {
+                                Tile::Path
+                            }
+                        }
                         _ => unreachable!(),
                     })
                     .collect()
@@ -67,7 +100,7 @@ impl Map {
             y: 0,
         };
 
-        let mut end_index = tiles
+        let end_index = tiles
             .last()
             .unwrap()
             .iter()
@@ -82,36 +115,28 @@ impl Map {
         Map { tiles, start, end }
     }
 
-    fn get(&self, position: &Coordinates) -> Option<Tile> {
-        if position.x < 0
-            || position.y < 0
-            || position.x >= self.tiles.len()
-            || position.y >= self.tiles.len()
-        {
-            None
-        } else {
-            Some(self.tiles[position.y][position.x].clone())
-        }
+    fn get(&self, position: &Coordinates) -> &Tile {
+        &self.tiles[position.y][position.x]
     }
 }
 
 #[test]
 fn test_from_lines() {
-    let example = Map::from_lines(_example());
+    let example = Map::from_lines(_example(), true);
     assert_eq!(example.tiles.len(), 23);
     assert_eq!(example.tiles[0].len(), 23);
     assert_eq!(example.start, Coordinates { x: 1, y: 0 });
     assert_eq!(example.end, Coordinates { x: 21, y: 22 });
 
-    assert_eq!(example.get(&Coordinates { x: 0, y: 0 }), Some(Tile::Forest));
-    assert_eq!(example.get(&Coordinates { x: 1, y: 1 }), Some(Tile::Path));
+    assert_eq!(example.get(&Coordinates { x: 0, y: 0 }), &Tile::Forest);
+    assert_eq!(example.get(&Coordinates { x: 1, y: 1 }), &Tile::Path);
     assert_eq!(
         example.get(&Coordinates { x: 10, y: 3 }),
-        Some(Tile::Slope(Direction::Right))
+        &Tile::Slope(Direction::Right)
     );
     assert_eq!(
         example.get(&Coordinates { x: 3, y: 4 }),
-        Some(Tile::Slope(Direction::Down))
+        &Tile::Slope(Direction::Down)
     );
 }
 
@@ -199,6 +224,7 @@ impl Coordinates {
     }
 }
 
+#[derive(Clone)]
 struct Walker {
     positions: HashSet<Coordinates>,
     current: Coordinates,
@@ -212,36 +238,49 @@ impl Walker {
         }
     }
 
-    fn next(&self, map: &Map) -> Vec<Walker> {
-        let mut result = vec![];
-        for pos in self.current.next() {
-            if !self.positions.contains(&pos) {
-                match map.get(&pos) {
-                    None | Some(Tile::Forest) => {}
-                    Some(Tile::Path) => {
-                        let mut positions = self.positions.clone();
-                        positions.insert(pos.clone());
-                        result.push(Walker {
-                            current: pos,
-                            positions,
-                        })
-                    }
-                    Some(Tile::Slope(d)) => {
-                        let mut positions = self.positions.clone();
-                        let bottom = pos.towards(d).unwrap();
-                        if !self.positions.contains(&bottom) {
-                            positions.insert(pos);
-                            positions.insert(bottom.clone());
-                            result.push(Walker {
-                                current: bottom,
-                                positions,
-                            })
-                        }
-                    }
-                }
+    fn next(self, map: &Map) -> Vec<Walker> {
+        let actual_next_iter = self
+            .current
+            .next()
+            .into_iter()
+            .filter(|pos| &Tile::Forest != map.get(pos) && !self.positions.contains(pos));
+
+        let mut result: Vec<Walker> = vec![];
+        let mut to_remove = vec![];
+        let mut actual_next = vec![];
+
+        for (i, pos) in actual_next_iter.enumerate() {
+            if i != 0 {
+                result.push(self.clone());
             }
+            actual_next.push(pos);
+        }
+        if actual_next.len() > 0 {
+            result.push(self);
         }
 
+        for i in 0..actual_next.len() {
+            let pos = actual_next.pop().unwrap();
+            match map.get(&pos) {
+                Tile::Path => {
+                    result[i].positions.insert(pos.clone());
+                    result[i].current = pos;
+                }
+                Tile::Slope(d) => {
+                    let bottom = pos.towards(d.clone()).unwrap();
+                    if result[i].positions.insert(bottom.clone()) {
+                        result[i].positions.insert(pos);
+                        result[i].current = bottom;
+                    } else {
+                        to_remove.push(i);
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+        for i in to_remove.iter().rev() {
+            result.remove(*i);
+        }
         result
     }
 
@@ -255,13 +294,13 @@ impl Walker {
                     if self.positions.contains(&coords) {
                         "O"
                     } else {
-                        match map.get(&coords) {
-                            None | Some(Tile::Forest) => "#",
-                            Some(Tile::Path) => ".",
-                            Some(Tile::Slope(Direction::Left)) => "<",
-                            Some(Tile::Slope(Direction::Right)) => ">",
-                            Some(Tile::Slope(Direction::Up)) => "^",
-                            Some(Tile::Slope(Direction::Down)) => "v",
+                        match tile {
+                            Tile::Forest => "#",
+                            Tile::Path => ".",
+                            Tile::Slope(Direction::Left) => "<",
+                            Tile::Slope(Direction::Right) => ">",
+                            Tile::Slope(Direction::Up) => "^",
+                            Tile::Slope(Direction::Down) => "v",
                         }
                     }
                 })
@@ -292,12 +331,22 @@ fn find_paths(map: &Map) -> Vec<HashSet<Coordinates>> {
 
 #[test]
 fn test_find_paths() {
-    let example = Map::from_lines(_example());
-    let paths = find_paths(&example);
+    let example_slippery = Map::from_lines(_example(), true);
+    let slippery_paths = find_paths(&example_slippery);
+    let slippery_paths_lengths = slippery_paths
+        .iter()
+        .map(|p| p.len() - 1)
+        .collect::<HashSet<_>>();
 
-    let paths_lengths = paths.iter().map(|p| p.len() - 1).collect::<HashSet<_>>();
-    println!("{:?}", paths_lengths);
+    assert_eq!(6, slippery_paths.len());
+    assert_eq!(94, *slippery_paths_lengths.iter().max().unwrap());
 
-    assert_eq!(6, paths.len());
-    assert_eq!(94, *paths_lengths.iter().max().unwrap());
+    let example_sticky = Map::from_lines(_example(), false);
+    let sticky_paths = find_paths(&example_sticky);
+    let sticky_paths_lengths = sticky_paths
+        .iter()
+        .map(|p| p.len() - 1)
+        .collect::<HashSet<_>>();
+
+    assert_eq!(154, *sticky_paths_lengths.iter().max().unwrap());
 }
