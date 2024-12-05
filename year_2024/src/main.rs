5d5076bf7c88719d71f919a5fa5d14ac45b26a51
Germain Chazot <germain@edited.com>
2024-12-05 19:21:17 +0100

    2024 day 3 part 1

--- year_2024/src/main.rs
+++ year_2024/src/main.rs
@@ -1,8 +1,9 @@
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
-    ;
     println!("Day  1: {}", day1::execute());
     println!("Day  2: {}", day2::execute());
+    println!("Day  3: {}", day3::execute());
 }
