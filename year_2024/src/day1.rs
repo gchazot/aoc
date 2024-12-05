e20946b1b26dcb25a50a20c053b6670c02faec4d
Germain Chazot <germain@edited.com>
2024-12-05 14:40:31 +0100

    2024 Day 1 part 2

--- year_2024/src/day1.rs
+++ year_2024/src/day1.rs
@@ -4,8 +4,9 @@ pub fn execute() -> String {
     let mine = aoc_utils::read_lines("input/day1.txt");
     let (list1, list2) = build_lists(mine.as_slice());
     let part1 = total_distance(list1.as_slice(), list2.as_slice());
+    let part2 = similarity_score(list1.as_slice(), list2.as_slice());
 
-    format!("{}", part1)
+    format!("{} {}", part1, part2)
 }
 
 fn build_lists(lines: &[String]) -> (Vec<u32>, Vec<u32>) {
@@ -22,6 +23,16 @@ fn build_lists(lines: &[String]) -> (Vec<u32>, Vec<u32>) {
     (list1, list2)
 }
 
+fn count_in_list(number: u32, list: &[u32]) -> u32 {
+    let mut result = 0;
+    for &i in list {
+        if i == number {
+            result += 1;
+        }
+    }
+    result
+}
+
 fn total_distance(list1: &[u32], list2: &[u32]) -> u32 {
     assert_eq!(list1.len(), list2.len());
     let mut sorted1 = Vec::from(list1);
@@ -34,13 +45,18 @@ fn total_distance(list1: &[u32], list2: &[u32]) -> u32 {
         .map(|(a, b)| a.abs_diff(b))
         .sum()
 }
+
+fn similarity_score(list1: &[u32], list2: &[u32]) -> u32 {
+    list1.iter().map(|&a| a * count_in_list(a, list2)).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_mine() {
-        assert_eq!(execute(), String::from("580"));
+        assert_eq!(execute(), String::from("1506483 23126924"));
     }
 
     #[test]
@@ -58,6 +74,24 @@ mod tests {
         let (list1, list2) = build_lists(_example().as_slice());
         assert_eq!(total_distance(list1.as_slice(), list2.as_slice()), 11);
     }
+
+    #[test]
+    fn test_count_in_list() {
+        let (_list1, list2) = build_lists(_example().as_slice());
+
+        assert_eq!(count_in_list(3, &list2), 3);
+        assert_eq!(count_in_list(4, &list2), 1);
+        assert_eq!(count_in_list(2, &list2), 0);
+        assert_eq!(count_in_list(1, &list2), 0);
+        assert_eq!(count_in_list(3, &list2), 3);
+    }
+
+    #[test]
+    fn test_similarity_score() {
+        let (list1, list2) = build_lists(_example().as_slice());
+        assert_eq!(similarity_score(list1.as_slice(), list2.as_slice()), 31);
+    }
+
     fn _example() -> Vec<String> {
         vec![
             String::from("3   4"),
