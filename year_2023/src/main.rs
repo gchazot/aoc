729ea9e925055dfcdc44222fd91a3f2e63e00361
Germain Chazot <germain@edited.com>
2024-12-08 17:23:34 +0100

    2023 Day 25 part 1

--- year_2023/src/main.rs
+++ year_2023/src/main.rs
@@ -15,6 +15,7 @@ mod day21;
 mod day22;
 mod day23;
 mod day24;
+mod day25;
 mod day3;
 mod day4;
 mod day5;
@@ -48,4 +49,5 @@ fn main() {
     day22::execute();
     day23::execute();
     day24::execute();
+    day25::execute();
 }
