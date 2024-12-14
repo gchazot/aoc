7563f4d84916a5678c75ebb9563f2b9c4f85c72a
Germain Chazot <germain@edited.com>
2024-12-14 15:31:01 +0100

    2024 Day 14

--- year_2024/src/main.rs
+++ year_2024/src/main.rs
@@ -6,6 +6,7 @@ mod day10;
 mod day11;
 mod day12;
 mod day13;
+mod day14;
 mod day4;
 mod day5;
 mod day6;
@@ -27,4 +28,5 @@ fn main() {
     println!("Day 11: {}", day11::execute());
     println!("Day 12: {}", day12::execute());
     println!("Day 13: {}", day13::execute());
+    println!("Day 14: {}", day14::execute());
 }
